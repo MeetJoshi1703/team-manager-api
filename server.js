e83bc783700c55734055cb8c1bc839e197adff39
MeetJoshi1703 <98758359+MeetJoshi1703@users.noreply.github.com>
2024-04-03 16:56:58 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import userRoutes from './api/routes/userRoutes.js';
+import teamRoutes from './api/routes/teamRoutes.js';
+import connectDB from './api/config/db.js'; 
+import cors from 'cors';
+
+const port = process.env.PORT || 8000;
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
+
+app.use('/api/users',userRoutes);
+app.use('/api/teams',teamRoutes);
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(port,()=>{
+    console.log(`Server is runnning on port ${port}`);
+});
